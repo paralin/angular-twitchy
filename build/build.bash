0e5c64bd40598fa8ae7a24c7032677d344e36c23
Christian Stewart <kidovate@gmail.com>
2015-06-01 20:09:39 -0700

    Add more

--- build/build.bash
+++ build/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./twitch-chat-emoticon-sprites/ && node generate.js -js wagamamatv draskyl beyondthesummit moonmeander ixmike88 monkeys_forever thegdstudio synderen sajedene arteezy dota2ti sing_sing && cd -
+cd ./twitch-chat-emoticon-sprites/ && node generate.js -js wagamamatv draskyl beyondthesummit moonmeander ixmike88 monkeys_forever thegdstudio synderen sajedene arteezy dota2ti sing_sing sonyatheevil forsenlol trumpsc && cd -
 cp ../src/twitchy.js ../dist/scripts/twitchy.js
 sed -e "s/{{EMOTES}}/$(cat ./twitch-chat-emoticon-sprites/icons.json)/" -i ../dist/scripts/twitchy.js
 sed -e "s/{{EMOTESWIDTHS}}/$(cat ./twitch-chat-emoticon-sprites/iconswidths.json)/" -i ../dist/scripts/twitchy.js

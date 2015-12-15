fdc4f2c5b74434fb54353d3f91a6cd7ac590f543
Christian Stewart <christian@paral.in>
2015-12-15 18:48:35 -0500

    fpl related changes

--- build/test.bash
+++ build/test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./twitch-chat-emoticon-sprites/ && node generate.js -njs wagamamatv draskyl moonmeander ixmike88 thegdstudio synderen sajedene arteezy sing_sing forsenlol trumpsc admiralbulldog lirik feardarkness sumaildoto egm arise_3012 dendi faceittv dotademon ppd universedota pajkattdota && cd -
+cd ./twitch-chat-emoticon-sprites/ && node generate.js -njs wagamamatv draskyl beyondthesummit moonmeander ixmike88 monkeys_forever thegdstudio synderen sajedene arteezy dota2ti sing_sing sonyatheevil forsenlol trumpsc admiralbulldog lirik feardarkness sumaildoto egm arise_3012 dendi faceittv dotademon ppd universedota && cd -
 cp ../src/twitchy.js ../dist/scripts/twitchy.js
 sed -e "s/{{EMOTES}}/$(cat ./twitch-chat-emoticon-sprites/icons.json)/" -i ../dist/scripts/twitchy.js
 sed -e "s/{{EMOTESWIDTHS}}/$(cat ./twitch-chat-emoticon-sprites/iconswidths.json)/" -i ../dist/scripts/twitchy.js
